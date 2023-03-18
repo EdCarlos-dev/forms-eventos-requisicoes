02ef3ab5e818be86861fb044ef759d68c1b645e3
EdCarlos-dev <bio_edc@hotmail.com>
2023-03-17 21:38:47 -0300

    apostila07(forms-eventos-requisicoes): fim da secão 2.3 - componente  busca criado - componente busca utilizado pelo app

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import Busca from "./Busca"
+const App = () => {
+    return(
+
+        <div className="grid justify-content-center m-auto w-9 border-round border-1 border-400">
+            <div className="col-12">
+                <h1>Exibir </h1>
+            </div>
+            <Busca />
+        </div>
+    )
+}
+export default App
